0f6f92ecd85702ec8b66d6addbec2ba04b2bfd72
Николай Борель <borel.nikolai@yandex.ru>
2023-04-22 15:41:46 +0400

    6

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -19,6 +19,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
